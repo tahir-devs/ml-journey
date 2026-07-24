65d0f0c715bd2008c42df16e056c8b244f91a0f3
tahir-devs <janikamo465@gmail.com>
2026-07-24 15:55:10 +0500

    Update and rename veriable.py to variables.py

---
+++ My Practice & Consistently/variables.py
@@ -0,0 +1,22 @@
+
+#Variable
+#Sime Coding
+name = "Tahir"
+print(name)
+age = 16
+print(age)
+Class = "9th"
+print(Class)
+print("==================")
+#Profesnationl coding
+name = "Ch.Tahir"
+age = 16
+Class = "9th"
+hobby = "Coding & Aloning"
+#Printing Data with (f) string
+print(f" Name : {name}\n Age : {age}\n Class : {Class}\n My Hobby : {hobby}")
+print("==================")
+#coping in variables
+car = "Elantra"
+a = car
+print(a)
